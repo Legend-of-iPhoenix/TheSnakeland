8bc295d194782dbc148fe4d0d30ccf79cb2fead0
_iPhoenix_ <32944537+Legend-of-iPhoenix@users.noreply.github.com>
2018-02-03 16:32:58 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 // Copyright _iPhoenix_
+var interval, isRunning = true;
 window.onload = function () {
-  setInterval(function () {
+  interval = setInterval(function () {
     var elements = document.getElementsByClassName('rainbow');
     for (var i = 0; i < elements.length; i++) {
       var span = elements[i];
@@ -16,4 +17,31 @@ window.onload = function () {
       span.innerHTML = innerString;
     }
   }, 50);
+}
+
+function toggleRainbow() {
+  if (isRunning) {
+    clearInterval(interval)
+    var elements = document.getElementsByClassName('rainbow');
+    for (var i = 0; i < elements.length; i++){
+      elements[i].innerHTML = elements[i].innerText;
+    }
+  } else {
+    interval = setInterval(function () {
+    var elements = document.getElementsByClassName('rainbow');
+    for (var i = 0; i < elements.length; i++) {
+      var span = elements[i];
+      var length = span.innerText.length;
+      var offset = span.id++;
+      span.id %= length + 1;
+      var innerString = '';
+      var length = span.innerText.length;
+      span.innerText.split('').forEach(function (char, index) {
+        var h = Math.floor((360 * (index + offset)) / length) % 360;
+        innerString += '<span style="color: hsl(' + h + ', 100%, 50%);">' + char + "</span>";
+      });
+      span.innerHTML = innerString;
+    }
+  }, 50);
+  }
 }
